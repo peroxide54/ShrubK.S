10b0e3708379e8e12225ad4de66453051073b915
ShrubKS <kirill.shrub@mail.ru>
2025-01-28 18:16:33 +0300

    Занятие 1

---
+++ занятие1..py
@@ -0,0 +1,7 @@
+
+full_name = input("Ваши фамилия, имя? ")
+age = input("Сколько Вам лет? ")
+place = input("Где вы живете? ")
+print("\nВаши фамилия, имя:", full_name)
+print("Ваш возраст:", age)
+print("Вы живете в:", place)
